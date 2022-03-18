64e13352e7014dcdb83a979ab0065abccbf50152
Олег Матшин <89175955650@mail.ru>
2022-03-18 22:23:14 +0500

    Top words from news count

--- Test_180322_2005.py
+++ Test_180322_2005.py
@@ -1,7 +1,20 @@
 import xml.etree.ElementTree as ET
+import collections
 
-def read_xml(xml_file, len_words=6, total_words=10):
-    pass
+
+def read_xml(xml_file, len_words=6, top_words=10):
+    parser = ET.XMLParser(encoding='utf-8')
+    tree = ET.parse(xml_file, parser)
+    root = tree.getroot()
+    words_list = []
+    description = root.findall('channel/item/description')
+    for item in description:
+        words_list.extend(item.text.split())
+    words_list = [word for word in words_list if len(word) > len_words]
+    counter_words = collections.Counter(words_list)    
+    # print(words_list)
+    # print(counter_words)
+    print(counter_words.most_common(top_words))
 
 if __name__ == '__main__':
     read_xml('test.xml')

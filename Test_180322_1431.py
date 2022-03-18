5a90659dd43a57e744cd409a9f1b85a1adace4f3
Олег Матшин <89175955650@mail.ru>
2022-03-18 19:57:12 +0500

    Find sum of words in news and rename the files of date

--- Test_180322_1431.py
+++ Test_180322_1431.py
@@ -1,9 +1,15 @@
-from pydoc import importfile
 import json
+import collections
 
-def read_json(file_path):
-    pass
-
+def read_json(file_path, max_len_word=6, top_words=10):
+    with open(file_path, encoding='utf-8') as news_file:
+        news = json.load(news_file)
+        description_words = []
+        for item in news['rss']['channel']['items']:
+            description = [word for word in item['description'].split(' ') if len(word) > max_len_word]
+            description_words.extend(description)
+            counter_words = collections.Counter(description_words)    
+        print(counter_words.most_common(top_words))
 
 if __name__ == '__main__':
     read_json('test.json')
